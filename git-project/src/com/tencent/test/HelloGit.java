257ff517ce1df7c4b62d6f53d13a58bdc93286ec
leichy <1626639901@qq.com>
2018-10-29 20:29:23 +0800

    删除idea修改

--- git-project/src/com/tencent/test/HelloGit.java
+++ git-project/src/com/tencent/test/HelloGit.java
@@ -6,6 +6,5 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("通过idea修改");
         System.out.println("通过克隆项目修改");
-        System.out.println("通过idea修改");
     }
 }
